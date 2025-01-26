171ec4a87c2e8fcc4dec3ee040e23665287c0be0
Growthen <arielandrerevilla@gmail.com>
2025-01-25 22:09:01 -0500

    Agregar Items a fabric 1.20.x

--- src/main/java/net/grow/tutorialmod/TutorialMod.java
+++ src/main/java/net/grow/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.grow.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.grow.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 
 }
