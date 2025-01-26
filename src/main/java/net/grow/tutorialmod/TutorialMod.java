057558d6192c890c3b8f8154cc8c2e13924e1245
Growthen <arielandrerevilla@gmail.com>
2025-01-26 01:30:27 -0500

    Agregando Bloques

--- src/main/java/net/grow/tutorialmod/TutorialMod.java
+++ src/main/java/net/grow/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.grow.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.grow.tutorialmod.block.ModBlocks;
 import net.grow.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +15,7 @@ public class TutorialMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 
 }
